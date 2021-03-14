aee199eef1cee0beef9aa11088bbc993fe46168d
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2021-03-14 13:43:42 +0200

    Published package to pub.dev

--- lib/bdaya_fcm_handler.dart
+++ lib/bdaya_fcm_handler.dart
@@ -1,51 +1,63 @@
 library bdaya_fcm_handler;
 
 import 'dart:async';
+import 'dart:io';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:rxdart/rxdart.dart' as rx;
+import 'package:flutter/foundation.dart';
 
+/// Specifies the notification source
+/// OnMessage: messages from [FirebaseMessaging.onMessage]
+/// OnMessageOpenedApp: messages from [FirebaseMessaging.onMessageOpenedApp]
+/// OnBackgroundMessage: messages from [FirebaseMessaging.onBackgroundMessage]
 enum NotificationSource { OnMessage, OnBackgroundMessage, OnMessageOpenedApp }
 
-Future<void> handleBackgroundNotifs(RemoteMessage message) async {
-  fcmServiceFinder
-      ?.call()
-      ?._raiseEvent(NotificationSource.OnBackgroundMessage, message);
+Future<void> _handleBackgroundNotifs(RemoteMessage message) async {
+  final finder = fcmServiceFinder;
+  if (finder != null) {
+    finder()?._raiseEvent(NotificationSource.OnBackgroundMessage, message);
+  }
 }
 
 /// assign this to find and call the FCM service
-FCMService Function()? fcmServiceFinder;
+FCMService? Function()? fcmServiceFinder;
 
-class CombinedUserToken {
-  final String userId;
-  final String token;
-
-  CombinedUserToken._(this.userId, this.token);
-}
-
+/// how the message listeners should look like
 typedef NotificationHandlerFunc = void Function(
-    NotificationSource src, RemoteMessage message);
+  NotificationSource src,
+  RemoteMessage message,
+);
 
-/// First: register this service (using normal [Get.put])
-/// Second: register your listeners using [registerSubscriber]
-/// Third: call doInit
+/// Request FCM settings for web,iOs and macOs
+typedef FCMRequestFunc = Future<NotificationSettings> Function();
+
+/// 1: register this service using a DI package (e.g. GetX)
+/// 2: set fcmServiceFinder to a function that returns the [FCMService] singleton (e.g. Get.find<FCMService>)
+/// 3: register your listeners using [registerSubscriber]
+/// 4: call [doInit] in your splash or onboarding screen
 class FCMService {
   final Map<String, StreamSubscription> _streamSubs = {};
 
   final _notificationSubscribers = <NotificationHandlerFunc>{};
 
-  /// A helper stream that combines the current user with its token
-  // Stream<CombinedUserToken> get combinedAuthTokenStream =>
-  //     rx.Rx.combineLatest2<User, String, CombinedUserToken>(
-  //       FirebaseAuth.instance.authStateChanges(),
-  //       FirebaseMessaging.instance.onTokenRefresh,
-  //       (a, b) => CombinedUserToken._(a.uid, b),
-  //     );
-
   void _raiseEvent(NotificationSource src, RemoteMessage message) {
     for (var sub in _notificationSubscribers) {
       sub(src, message);
     }
+  }
+
+  /// Convenience stream for [FirebaseMessaging.onTokenRefresh]
+  Stream<String> get onTokenRefresh =>
+      FirebaseMessaging.instance.onTokenRefresh;
+
+  /// Used to get the FCM token and logs the result
+  Future<String?> getToken({String? vapidKey, bool logResult = true}) async {
+    final token = await FirebaseMessaging.instance.getToken(vapidKey: vapidKey);
+    if (!logResult) {
+      final borders = "=================================================";
+      print('$borders\n FCM Token received: ${(token ?? '--')}\n$borders');
+    }
+    return token;
   }
 
   /// Registers a notification subscriber
@@ -58,30 +70,37 @@ class FCMService {
     _notificationSubscribers.remove(handler);
   }
 
-  Future<RemoteMessage?> doInit() async {
-    // final settings = await FirebaseMessaging.instance.requestPermission(
-    //   alert: true,
-    //   announcement: false,
-    //   badge: true,
-    //   carPlay: false,
-    //   criticalAlert: false,
-    //   provisional: false,
-    //   sound: true,
-    // );
+  Future<RemoteMessage?> doInit([FCMRequestFunc? requestFunc]) async {
+    bool canUseFCM = true;
+    if (kIsWeb || Platform.isIOS || Platform.isMacOS) {
+      final settings = await requestFunc?.call();
+      if (settings != null) {
+        print('User granted permission: ${settings.authorizationStatus}');
+        if (settings.authorizationStatus == AuthorizationStatus.denied ||
+            settings.authorizationStatus == AuthorizationStatus.notDetermined) {
+          canUseFCM = false;
+        }
+      }
+    }
 
-    final initMessage = await FirebaseMessaging.instance.getInitialMessage();
+    if (canUseFCM) {
+      final initMessage = await FirebaseMessaging.instance.getInitialMessage();
 
-    _streamSubs['onMessage'] = FirebaseMessaging.onMessage.listen((event) {
-      _raiseEvent(NotificationSource.OnMessage, event);
-    });
-    FirebaseMessaging.onBackgroundMessage(handleBackgroundNotifs);
-    _streamSubs['onMessageOpenedApp'] =
-        FirebaseMessaging.onMessageOpenedApp.listen((event) {
-      _raiseEvent(NotificationSource.OnMessageOpenedApp, event);
-    });
-    return initMessage;
+      _streamSubs['onMessage'] = FirebaseMessaging.onMessage.listen((event) {
+        _raiseEvent(NotificationSource.OnMessage, event);
+      });
+      FirebaseMessaging.onBackgroundMessage(_handleBackgroundNotifs);
+      _streamSubs['onMessageOpenedApp'] =
+          FirebaseMessaging.onMessageOpenedApp.listen((event) {
+        _raiseEvent(NotificationSource.OnMessageOpenedApp, event);
+      });
+      return initMessage;
+    } else {
+      return null;
+    }
   }
 
+  /// Call this to dispose the service, cancels all streams and removes all subscribers
   Future<void> onClose() async {
     for (var item in _streamSubs.values) {
       await item.cancel();
